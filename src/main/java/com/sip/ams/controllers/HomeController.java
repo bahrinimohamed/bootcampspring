66331709945e1903912c36aa3e20ea71143530a1
mohamed bahrini <mohamed.ba7rini.87@gmail.com>
2022-02-06 15:04:41 +0100

    first project

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/info")
+	public String info()
+	 { System.out.println("methode info");
+	return "home/info";
+	 }
+	@RequestMapping("/affichage")//ce que je tape dans l'url
+	public String affiche()
+	{
+		return "home/affichage";//le nom de la vue sous template/home
+	}
+}
