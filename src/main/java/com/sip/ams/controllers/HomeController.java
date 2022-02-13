dbfc7b7cf58cbe9f151c6d7601ca7dba78c1d854
mohamed bahrini <mohamed.ba7rini.87@gmail.com>
2022-02-13 10:21:45 +0100

    nouvelle_modif

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,18 +1,48 @@
+
 package com.sip.ams.controllers;
 
+
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/info")
-	public String info()
-	 { System.out.println("methode info");
-	return "home/info";
-	 }
-	@RequestMapping("/affichage")//ce que je tape dans l'url
-	public String affiche()
-	{System.out.println("methode info");
-		return "home/affichage";//le nom de la vue sous template/home
+	
+	@PostMapping("/result")
+	@ResponseBody
+	public String resultat(@RequestParam("nom") String n, @RequestParam("email") String e)
+	{
+		return "Vous avez taper un nom : " +n +" un email : "+e;
 	}
+	
+	@RequestMapping("/information") // ce que je tape dans l'url
+	public String info(Model model)
+	{
+		ArrayList<String> names = new ArrayList<>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("Spring");
+		model.addAttribute("names", names);
+		String formation = "Fullstack";
+		System.out.println("Méthode info");
+		
+		
+		model.addAttribute("workshop",formation);
+		
+		return "home/info";  // le nom de la vue sous template /home
+	}
+	
+	@RequestMapping("/affichage")
+	public String affiche()
+	{
+		System.out.println("Méthode affichage");
+		return "home/affichage";
+	}
+
 }
